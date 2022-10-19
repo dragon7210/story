271974c55f15df429d7aeedc40540d1396bb10be
dragon <isanoguchi721@gmail.com>
2022-10-18 20:35:36 -0700

    10-18-20

--- src/pages/fourth.js
+++ src/pages/fourth.js
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { X1, X } from "../constant";
 
 const Fourth = () => {
+  const [lastName, setLastName] = useState("");
   const SecondTops = useSelector((state) => state.SecondTop).value;
   const SecondBottoms = useSelector((state) => state.SecondBottom).value;
   const FirstTops = useSelector((state) => state.FirstTop).value;
   const FirstBottoms = useSelector((state) => state.FirstBottom).value;
-  console.log(FirstTops, SecondTops);
+  const ChangeCharts = useSelector((state) => state.ChangeChart).value;
+  useEffect(() => {
+    if (ChangeCharts.length !== 0) {
+      let abc = ChangeCharts.map((a) => a);
+      const temp = abc.sort((a, b) => b.index - a.index)[0].name;
+      setLastName(temp);
+    }
+  }, [ChangeCharts]);
   return (
     <>
       <div className="top">
@@ -38,9 +47,12 @@ const Fourth = () => {
                         (element1) =>
                           element1.col === colIndex &&
                           element1.row === rowIndex && (
-                            <button className="mileButton">
-                              {element1.name}
-                            </button>
+                            <>
+                              <button className="mileButton">
+                                {element1.name}
+                              </button>
+                              {lastName === element1.name ? "flag" : <></>}
+                            </>
                           )
                       )}
                     </td>
@@ -81,9 +93,17 @@ const Fourth = () => {
                         (element1) =>
                           element1.col === colIndex &&
                           element1.row === rowIndex && (
-                            <button className="chalButton">
-                              {element1.nameBottom}
-                            </button>
+                            <>
+                              <button className="chalButton">
+                                {element1.nameBottom}
+                              </button>
+                              {lastName.slice(0, lastName.length - 6) ===
+                              element1.nameBottom ? (
+                                "flag"
+                              ) : (
+                                <></>
+                              )}
+                            </>
                           )
                       )}
                     </td>
---
+++ src/pages/test.js
@@ -0,0 +1,7 @@
+let x = [
+  { index: 2, name: "Factor Abottom" },
+  { index: 5, name: "Factor Gbottom" },
+  { index: 3, name: "Factor Ebottom" },
+];
+
+console.log(x.length);
